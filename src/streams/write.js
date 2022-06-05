e2b18e32f5567665645fd11e2ba32c058bc1d146
Anna <annabutkova94@gmail.com>
2022-06-06 01:30:10 +0500

    feat: streams

--- src/streams/write.js
+++ src/streams/write.js
@@ -1,3 +1,26 @@
+import * as fs from 'fs';
+import * as path from 'path';
+import * as readline from 'readline';
+
+const __dirname = path.resolve(path.dirname(""));
+const DIST = path.join(__dirname, '/src/streams/files/fileToWrite.txt');
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+});
+
+
 export const write = async () => {
-    // Write your code here 
+
+    const stream = rl.on('line', line => {
+        line = line + '\n';
+        fs.writeFile(DIST, line, { flag: 'a+' }, function (error) {
+            if (error) throw error;
+        });
+    })
+
+    process.stdin.pipe(stream);
 };
+
+write();

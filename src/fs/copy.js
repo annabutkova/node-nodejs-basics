3e23fdaf4a275c4fb5a73d89a8a4e17858c7b4d5
Anna <annabutkova94@gmail.com>
2022-06-05 23:05:13 +0500

    3 task done

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,3 +1,33 @@
+import * as fs from 'fs/promises';
+import * as path from 'path';
+const __dirname = path.resolve(path.dirname(""));
+const SRC = path.join(__dirname, '/src/fs/files/');
+const DEST_DIR = path.join(__dirname, '/src/fs/files-copy/');
+
 export const copy = async () => {
-    // Write your code here 
+
+    try {
+        const files = await fs.readdir(SRC);
+
+        return await fs.mkdir(DEST_DIR, { recursive: false })
+            .then(copyFiles())
+            .catch((error) => {
+                console.log(error);
+                console.error(new Error('FS operation failed'));
+            })
+
+        async function copyFiles() {
+            for (const file of files) {
+                fs.copyFile(`${SRC}/${file}`, `${DEST_DIR}/${file}`);
+            }
+        }
+
+    }
+    catch (error) {
+        console.error(new Error('FS operation failed'));
+    }
+
+
 };
+
+copy();
--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,3 +1,17 @@
+import * as fs from 'node:fs/promises';
+import * as path from 'path';
+const __dirname = path.resolve(path.dirname(""));
+const OLD = path.join(__dirname, '/src/fs/files/wrongFilename.txt');
+const NEW = path.join(__dirname, '/src/fs/files/properFilename.md');
+
+
+ 
 export const rename = async () => {
-    // Write your code here 
+    try {
+        await fs.rename(OLD, NEW);
+      } catch (error) {
+        console.error(new Error('FS operation failed'));
+      }
 };
+
+rename();

a6eed750a162432f8a4f894976e8db4f0e6d5c12
Anna <annabutkova94@gmail.com>
2022-06-05 23:44:54 +0500

    feat: delete task

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,3 +1,14 @@
+import * as fs from 'node:fs/promises';
+import * as path from 'path';
+const __dirname = path.resolve(path.dirname(""));
+const file = path.join(__dirname, '/src/fs/files/fileToRemove.txt');
+
 export const remove = async () => {
-    // Write your code here 
+    try {
+        await fs.unlink(file);
+    } catch (error) {
+        console.error(new Error('FS operation failed'));
+    }
 };
+
+remove();

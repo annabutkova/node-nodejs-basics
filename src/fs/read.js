4edca6c6983d5372b400aa15a43f709755c502a8
Anna <annabutkova94@gmail.com>
2022-06-06 00:01:53 +0500

    feat: read task

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,3 +1,17 @@
+import * as fs from 'fs/promises';
+import * as path from 'path';
+const __dirname = path.resolve(path.dirname(""));
+const SRC = path.join(__dirname, '/src/fs/files/fileToRead.txt');
+
+
 export const read = async () => {
-    // Write your code here 
+    try {
+        const text = await fs.readFile(SRC);
+        console.log(text.toString());
+    }
+    catch (error) {
+        console.error(new Error('FS operation failed'));
+    }
 };
+
+read();
